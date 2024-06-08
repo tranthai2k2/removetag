fb2b0c11b157438c485a5612e0976799cd678a6f
DESKTOP-EPP7KRG\HP15 <tranthai26052k2@gmail.com>
2024-06-08 14:09:18 +0700

    v2

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+const wantRemove = {
+    hairStyle: [
+        "Hair Style", "Long Hair", "Short Hair", "Ponytail", 
+        "Braided Hair", "Twin Tails", "Bangs", "Messy Hair", 
+        "Bob Cut", "Curly Hair", "Hair Between Eyes", "Hime Cut", 
+        "Side Bangs", "Fringe", "Pixie Cut", "Mohawk", "Undercut", 
+        "Bald", "Ombre Hair", "ponytail", "braid", "single braid"
+    ],
+    hairColor: [
+        "hair color", "blonde hair", "black hair", "brown hair", 
+        "red hair", "blue hair", "green hair", "purple hair", 
+        "pink hair", "white hair", "silver hair", "gray hair", 
+        "multicolored hair", "two tone hair", "ombre hair", 
+        "dyed hair", "pastel hair", "rainbow hair", "gradient hair", 
+        "neon hair", "metallic hair", "vibrant hair"
+    ],
+    eyesColor: [
+        "Black Eyes", "Blue Eyes", "Brown Eyes", "Gray Eyes", 
+        "Green Eyes", "Orange Eyes", "Pink Eyes", "Purple Eyes", 
+        "Red Eyes", "White Eyes", "Yellow Eyes"
+    ]
+};
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("retrieveTextBtn").addEventListener("click", function() {
         var textareaContent = document.getElementById("exampleFormControlTextarea1").value;
@@ -5,10 +27,57 @@ document.addEventListener("DOMContentLoaded", function() {
         var newStr = textareaContent.replace(/\d+\?/g, ",");
         var finalStr = newStr.replace(/ ?(\? ?)|\? /g, "");
         var finalStr = finalStr.replace(/\b\d+\b/g, '');
+        
+          // Lấy tất cả các thẻ có class "tag"
+    const tags = document.querySelectorAll('.tags-container .tag');
+    // Tạo một mảng để lưu giá trị của các thẻ
+    const tagValues = [];
 
+    // Lặp qua từng thẻ và lấy giá trị từ thuộc tính "data-value"
+    tags.forEach(function(tag) {
+        tagValues.push(tag.getAttribute('data-value'));
+    });
+   
+    const textAreaValueWantRemove = document.getElementById('texteraWantRemove').value;
+    
+    // Chuyển đổi giá trị từ textarea thành mảng
+ 
+
+
+
+
+    // let element = document.getElementById("texteraout");
+    let string = finalStr;
+    for (let i = 0; i < tagValues.length; i++) {
+        // console.log(tagValues[i]);
+        string = removeTags(string,wantRemove,tagValues[i])
         document.getElementById("out").style.visibility = "visible";
         document.getElementById("texteraout").textContent = "";
-        document.getElementById("texteraout").textContent = finalStr;
+        document.getElementById("texteraout").textContent= string ;
+    }
+    let textAreaArray =  textAreaValueWantRemove.split(" , ")
+    .map(item => item.trim().replace(/,/g, ''))
+    .filter(item => item.length > 0);
+    if (textAreaArray.length > 0) {
+      if (textAreaArray.some(item => string.includes(item))) {
+          // Nếu chuỗi string chứa ít nhất một phần tử thuộc textAreaArray
+          // Xóa các phần tử thuộc textAreaArray khỏi chuỗi string
+          textAreaArray.forEach(item => {
+              string = string.replace(item, '');
+          });
+         let cleanedString = string.split(/,\s*/).filter(item => item.trim() !== "").join(", ");
+         document.getElementById("out").style.visibility = "visible";
+        document.getElementById("texteraout").textContent = "";
+        document.getElementById("texteraout").textContent= cleanedString;
+      } else {
+          console.log("Chuỗi không chứa phần tử thuộc textAreaArray");
+      }
+
+  }
+   
+        document.getElementById("out").style.visibility = "visible";
+        document.getElementById("texteraout").textContent = "";
+        document.getElementById("texteraout").textContent = string;
     });
 });
 
@@ -50,28 +119,7 @@ function removeTag(value) {
 }
 
 
-const wantRemove = {
-    hairStyle: [
-        "Hair Style", "Long Hair", "Short Hair", "Ponytail", 
-        "Braided Hair", "Twin Tails", "Bangs", "Messy Hair", 
-        "Bob Cut", "Curly Hair", "Hair Between Eyes", "Hime Cut", 
-        "Side Bangs", "Fringe", "Pixie Cut", "Mohawk", "Undercut", 
-        "Bald", "Ombre Hair", "ponytail", "braid"
-    ],
-    hairColor: [
-        "hair color", "blonde hair", "black hair", "brown hair", 
-        "red hair", "blue hair", "green hair", "purple hair", 
-        "pink hair", "white hair", "silver hair", "gray hair", 
-        "multicolored hair", "two tone hair", "ombre hair", 
-        "dyed hair", "pastel hair", "rainbow hair", "gradient hair", 
-        "neon hair", "metallic hair", "vibrant hair"
-    ],
-    eyesColor: [
-        "Black Eyes", "Blue Eyes", "Brown Eyes", "Gray Eyes", 
-        "Green Eyes", "Orange Eyes", "Pink Eyes", "Purple Eyes", 
-        "Red Eyes", "White Eyes", "Yellow Eyes"
-    ]
-};
+
 
 function removeTags(text, wantRemove, key) {
     const tags = wantRemove[key];
@@ -81,53 +129,9 @@ function removeTags(text, wantRemove, key) {
     return withoutTags.replace(/,\s*,/g, ','); // Loại bỏ dấu phẩy cuối cùng (nếu có)
 }
 
-document.getElementById('warningButton').addEventListener('click', function() {
-    // Lấy tất cả các thẻ có class "tag"
-    const tags = document.querySelectorAll('.tags-container .tag');
-    // Tạo một mảng để lưu giá trị của các thẻ
-    const tagValues = [];
-
-    // Lặp qua từng thẻ và lấy giá trị từ thuộc tính "data-value"
-    tags.forEach(function(tag) {
-        tagValues.push(tag.getAttribute('data-value'));
-    });
-   
-    const textAreaValueWantRemove = document.getElementById('texteraWantRemove').value;
-    
-    // Chuyển đổi giá trị từ textarea thành mảng
- 
-
-
-
-
-    let element = document.getElementById("texteraout");
-    let string = element.value;
-    for (let i = 0; i < tagValues.length; i++) {
-        // console.log(tagValues[i]);
-        string = removeTags(string,wantRemove,tagValues[i])
-        document.getElementById("out").style.visibility = "visible";
-        document.getElementById("texteraout").textContent = "";
-        document.getElementById("texteraout").textContent= string ;
-    }
-    let textAreaArray =  textAreaValueWantRemove.split(" , ")
-    .map(item => item.trim().replace(/,/g, ''))
-    .filter(item => item.length > 0);
-    if (textAreaArray.length > 0) {
-      if (textAreaArray.some(item => string.includes(item))) {
-          // Nếu chuỗi string chứa ít nhất một phần tử thuộc textAreaArray
-          // Xóa các phần tử thuộc textAreaArray khỏi chuỗi string
-          textAreaArray.forEach(item => {
-              string = string.replace(item, '');
-          });
-         let cleanedString = string.split(/,\s*/).filter(item => item.trim() !== "").join(", ");
-         document.getElementById("out").style.visibility = "visible";
-        document.getElementById("texteraout").textContent = "";
-        document.getElementById("texteraout").textContent= cleanedString;
-      } else {
-          console.log("Chuỗi không chứa phần tử thuộc textAreaArray");
-      }
-
-  }
+document.getElementById('warningButton').addEventListener('click', function() 
+{
+  
   
 
 
