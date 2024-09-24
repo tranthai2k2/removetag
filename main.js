10701d2c8880ddb06a1a5ca6e1fb9e6d71e0cba0
DESKTOP-EPP7KRG\HP15 <tranthai26052k2@gmail.com>
2024-09-24 23:02:36 +0700

    no message

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const wantRemove = {
-    hairStyles: [
-        "very short hair", "short hair", "medium hair", "long hair", "very long hair", "absurdly long hair", 
+    hairStyle: [
+        "Hair Style", "Long Hair", "Short Hair", "Ponytail", 
+        "Braided Hair", "Twin Tails", "Bangs", "Messy Hair", 
+        "Bob Cut", "Curly Hair", "Hair Between Eyes", "Hime Cut", 
+        "Side Bangs", "Fringe", "Pixie Cut", "Mohawk", "Undercut", 
+        "Bald", "Ombre Hair", "ponytail", "braid", "single braid","very short hair", "short hair", "medium hair", "long hair", "very long hair", "absurdly long hair", 
         "big hair", "bald", "bald girl", 
         // Hairstyles
         "bob cut", "inverted bob", "bowl cut", "buzz cut", "chonmage", "crew cut", "flattop", "okappa", "pixie cut", "undercut",
@@ -39,81 +43,62 @@ const wantRemove = {
         // Misc
         "alternate hairstyle", "hair down", "hair up", "asymmetrical hair", "sidecut", "blunt ends"
     ],
-    hairColors: [
-        // Specific colors
-        "aqua hair", "black hair", "blonde hair", "blue hair", "light blue hair", "dark blue hair", 
-        "brown hair", "light brown hair", "green hair", "dark green hair", "light green hair", 
-        "grey hair", "orange hair", "pink hair", "purple hair", "light purple hair", 
-        "red hair", "white hair",
-        // Multiple colors
-        "multicolored hair", "colored inner hair", "colored tips", "roots", "gradient hair", 
-        "patterned hair", "rainbow hair", "split-color hair", "spotted hair", "streaked hair", 
-        "two-tone hair",
+    hairColor: [
+        "hair color", "blonde hair", "black hair", "brown hair", 
+        "red hair", "blue hair", "green hair", "purple hair", 
+        "pink hair", "white hair", "silver hair", "gray hair", 
+        "multicolored hair", "two tone hair", "ombre hair", 
+        "dyed hair", "pastel hair", "rainbow hair", "gradient hair", 
+        "neon hair", "metallic hair", "vibrant hair","very short hair", "short hair", "medium hair", "long hair", "very long hair", "absurdly long hair", 
+        "big hair", "bald", "bald girl", 
+        // Hairstyles
+        "bob cut", "inverted bob", "bowl cut", "buzz cut", "chonmage", "crew cut", "flattop", "okappa", "pixie cut", "undercut",
+        // Medium hair
+        "flipped hair", "wolf cut", 
+        // Long hair
+        "hime cut", "mullet", 
+        // Tied hair
+        "bow-shaped hair", "shuangyaji", "braid", "braided bangs", "front braid", "side braid", "french braid", "cornrows", 
+        "crown braid", "dreadlocks", "single braid", "multiple braids", "twin braids", "low twin braids", "tri braids", "quad braids", 
+        "flower-shaped hair", "hair bun", "braided bun", "single hair bun", "double bun", "cone hair bun", "doughnut hair bun", 
+        "heart hair bun", "triple bun", "hair rings", "feixianji", "katsuyamamage", "single hair ring", "half updo", "half up braid", 
+        "half up half down braid", "one side up", "two side up", "low-braided long hair", "low-tied long hair", "mizura", "multi-tied hair", 
+        "nihongami", "ponytail", "folded ponytail", "front ponytail", "high ponytail", "short ponytail", "side ponytail", "split ponytail", 
+        "star-shaped hair", "topknot", "twintails", "low twintails", "short twintails", "uneven twintails", "tri tails", "quad tails", 
+        "quin tails", "twisted hair", 
+        // Tall hair
+        "afro", "huge afro", "beehive hairdo", "crested hair", "liangbatou", "pompadour", "quiff", "shouten pegasus mix mori",
+        // Hair texture
+        "curly hair", "drill hair", "twin drills", "tri drills", "hair flaps", "messy hair", "pointy hair", "ringlets", 
+        "spiked hair", "straight hair", "wavy hair",
+        // Front of the head
+        "bangs", "arched bangs", "asymmetrical bangs", "bangs pinned back", "blunt bangs", "braided bangs", "crossed bangs", 
+        "choppy bangs", "diagonal bangs", "dyed bangs", "fanged bangs", "hair over eyes", "hair over one eye", "long bangs", 
+        "parted bangs", "curtained hair", "wispy bangs", "short bangs", "swept bangs", "hair between eyes", "hair intakes", 
+        "single hair intake", "sidelocks", "asymmetrical sidelocks", "drill sidelocks", "low-tied sidelocks", "sidelocks tied back", 
+        "single sidelock", "widow's peak",
+        // Top of the head
+        "ahoge", "heart ahoge", "huge ahoge", "antenna hair", "heart antenna hair", "comb over", "hair pulled back", 
+        "hair slicked back", "mohawk", "oseledets",
+        // Back of the head
+        "lone nape hair",
+        // Head hair over the body
+        "hair bikini", "hair censor", "hair in own mouth", "hair over breasts", "hair over one breast", "hair over crotch", 
+        "hair over shoulder", "hair scarf",
         // Misc
-        "alternate hair color", "translucent hair"
+        "alternate hairstyle", "hair down", "hair up", "asymmetrical hair", "sidecut", "blunt ends"
     ],
-    eyeStyles: [
-        // Individual colors of the iris
-        "aqua eyes", "black eyes", "blue eyes", "brown eyes", "green eyes", "grey eyes", 
+
+    eyesColor: [
+        "Black Eyes", "Blue Eyes", "Brown Eyes", "Gray Eyes", 
+        "Green Eyes", "Orange Eyes", "Pink Eyes", "Purple Eyes", 
+        "Red Eyes", "White Eyes", "Yellow Eyes",  "aqua eyes", "black eyes", "blue eyes", "brown eyes", "green eyes", "grey eyes", 
         "orange eyes", "purple eyes", "pink eyes", "red eyes", "white eyes", "yellow eyes", 
         "amber eyes",
         // Multiple colors of the iris
-        "heterochromia", "multicolored eyes",
-        // Form of the iris
-        "mismatched irises", "dashed eyes", "Pac-man eyes", "ringed eyes",
-        // Pupils
-        // Colors of the pupils
-        "aqua pupils", "blue pupils", "brown pupils", "green pupils", "grey pupils", 
-        "orange pupils", "pink pupils", "purple pupils", "red pupils", "white pupils", 
-        "yellow pupils",
-        // Form of the pupils
-        "constricted pupils", "dilated pupils", "extra pupils", "horizontal pupils", 
-        "no pupils", "slit pupils", "symbol-shaped pupils", "diamond-shaped pupils", 
-        "flower-shaped pupils", "heart-shaped pupils", "star-shaped pupils", 
-        "solid circle pupils", "cross-shaped pupils", "x-shaped pupils", "mismatched pupils",
-        // Sclera
-        "blue sclera", "black sclera", "blank eyes", "bloodshot eyes", "green sclera", 
-        "mismatched sclera", "no sclera", "orange sclera", "red sclera", "yellow sclera",
-        // Around the eyes
-        "bags under eyes", "aegyo sal", "bruised eye", "flaming eyes", "glowing eyes", 
-        "glowing eye",
-        // More appearance
-        "Animal or inhuman eyes", "button eyes", "cephalopod eyes", "compound eyes", 
-        "horizontal pupils", "lens eye", "pixel eyes",
-        // Stylistic eyes, for emotion or comedy
-        "crazy eyes", "empty eyes", "dashed eyes", "heart-shaped eyes", "solid circle eyes", 
-        "jitome", "tareme", "tsurime", "sanpaku", "sparkling eyes",
-        // Series specific eyes
-        "Geass", "Sharingan", "Mangekyou Sharingan", "Rinnegan", "Byakugan",
-        // Other
-        "eye reflection", "text in eyes",
-        // Number of eyes
-        "missing eye", "one-eyed", "third eye", "extra eyes", "no eyes",
-        // Emotions and expressions
-        "> <", "X3", "XD", "DX", "O o", "0 0", "3 3", "6 9", "@ @", "^ ^", "^o^", 
-        "|_|", "= =", "+ +", ". .", "<o> <o>", "<|>_<|>", "blinking", "closed eyes", 
-        "wince", "one eye closed", ";<", ";>", ";p",
-        // Accessories for the eyes; Eyes covered
-        "covering own eyes", "hair over eyes", "hair over one eye", "bandage over one eye", 
-        "blindfold", "hat over eyes", "eyepatch", "eyelashes", "colored eyelashes", 
-        "fake eyelashes", "eyes visible through hair", "glasses", "makeup", "eyeliner", 
-        "eyeshadow", "mascara",
-        // Gazes
-        "eye contact", "looking afar", "looking around", "looking at another", 
-        "looking at breasts", "looking at hand", "looking at hands", "looking at mirror", 
-        "looking at phone", "looking at self", "looking at viewer", "looking at penis", 
-        "looking at pussy", "looking at crotch", "looking away", "looking back", 
-        "looking down", "looking outside", "looking over eyewear", 
-        "looking through own legs", "looking to the side", "looking up",
-        // Misc / Actions
-        "akanbe", "artificial eye", "glass eye", "mechanical eye", "asymmetrical eyes", 
-        "big eyes", "blind", "partially blind", "cross-eyed", "drawn on eyes", 
-        "eyeball", "eye beam", "eye poke", "eye pop", "eye trail", "googly eyes", 
-        "half-closed eyes", "lazy eye", "persona eyes", "pleading eyes", "rolling eyes", 
-        "shading eyes", "squinting", "staring", "uneven eyes", "unusually open eyes", 
-        "upturned eyes", "wall-eyed", "wide-eyed", "wince"
+        "heterochromia", "multicolored eyes"
+
     ]
-    
 };
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("retrieveTextBtn").addEventListener("click", function() {
